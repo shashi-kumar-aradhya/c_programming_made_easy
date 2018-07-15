dceac81365dc9cc8f456ee708b7552a96ee974a6
shashi <shashikumararadhya@gmail.com>
2018-07-15 20:16:34 +0530

    Merge conflict resolve

--- sorting_algorithms/bubble_sort.c
+++ sorting_algorithms/bubble_sort.c
@@ -17,11 +17,7 @@ int main()
 	int arr[MAX] = {0};
 	int i = 0, j = 0;
 	int comp = 0;
-<<<<<<< HEAD
 	int flag = 1;	//True, we assume swap is required
-=======
-	int flag = 1;
->>>>>>> f95855fc4b5350770d3beb09869ed8c85f4e07fc
 	printf("Enter the array elements : ");
 	for(i = 0; i < MAX; i++)
 		scanf("%d", &arr[i]);
@@ -31,19 +27,11 @@ int main()
 		printf("%d\t", arr[i]);
 
 	for(i = 0; i < MAX && flag; i++) {
-<<<<<<< HEAD
 		flag = 0;		//False, we assume swap is not required
 	   for(j = 0; j < MAX-i-1; j++) {
 		comp++;
 	      if( arr[j] > arr[j+1]) {
 		flag = 1;		//If swap is required, we need to continue one more iteration
-=======
-		flag = 0;
-	   for(j = 0; j < MAX-i-1; j++) {
-		comp++;
-	      if( arr[j] > arr[j+1]) {
-		flag = 1;
->>>>>>> f95855fc4b5350770d3beb09869ed8c85f4e07fc
 		swap(arr, j, j+1);
 	      }
 	   }
